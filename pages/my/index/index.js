40779f2544800968fb07d25aa76a688e4f4acd31
李晶晶 <1353281097@qq.com>
2020-04-22 01:12:02 +0800

    我的优惠券

--- pages/my/index/index.js
+++ pages/my/index/index.js
@@ -6,83 +6,83 @@ Page({
    * 页面的初始数据
    */
   data: {
-    userInfo:{},
+    userInfo: {},
     iconList: [{
-      url:"sale1.png",
+      url: "sale1.png",
       icon: 'cardboardfill',
       color: 'red',
       name: '待付款'
     }, {
-      url:"sale2.png",
+      url: "sale2.png",
       icon: 'recordfill',
       color: 'orange',
       name: '待发货'
     }, {
-      url:"sale3.png",
+      url: "sale3.png",
       icon: 'picfill',
       color: 'yellow',
       name: '待收货'
     }, {
-      url:"sale4.png",
+      url: "sale4.png",
       icon: 'noticefill',
       color: 'olive',
       name: '评价'
     }, {
-      url:"sale5.png",
+      url: "sale5.png",
       icon: 'noticefill',
       color: 'olive',
       name: '退款/售后'
     }],
     gridCol: 5,
-    recomlist:[],
-    openid:''
+    recomlist: [],
+    openid: ''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) { 
+  onLoad: function (options) {
     this.appgetrecomlist();
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {
+  onReady: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
-    if(wx.getStorageSync('openid')){
+  onShow: function () {
+    if (wx.getStorageSync('openid')) {
       this.setData({
-        openid:wx.getStorageSync('openid')
+        openid: wx.getStorageSync('openid')
       })
       this.getUserInfo();
     }
   },
   // 获取用户信息
-  getUserInfo(){
+  getUserInfo() {
     var _this = this;
     call.getData('/app/user/appgetuser', {
       OPENID: wx.getStorageSync('openid')
     }, function (res) {
-      if(res.state == "success"){
+      if (res.state == "success") {
         _this.setData({
-          userInfo:res.user
+          userInfo: res.user
         })
       }
       console.log(res);
     }, function () {})
   },
- 
+
 
   /**
    * 我的订单
    */
-  goOrder: function() {
+  goOrder: function () {
     if (!wx.getStorageSync('openid')) {
       wx.navigateTo({
         url: '/pages/login/login',
@@ -98,7 +98,7 @@ Page({
   /**
    * 个人信息
    */
-  goInfo: function() {
+  goInfo: function () {
     if (!wx.getStorageSync('openid')) {
       if (!wx.getStorageSync('openid')) {
         wx.navigateTo({
@@ -132,13 +132,13 @@ Page({
     })
   },
   // 下单指南与关于我们
-  goShowCase:function(e){ 
+  goShowCase: function (e) {
     wx.navigateTo({
-      url: '/pages/my/showcase/showcase?type='+ e.currentTarget.dataset.type
+      url: '/pages/my/showcase/showcase?type=' + e.currentTarget.dataset.type
     })
   },
   // 账户与安全
-  goSetings:function(){
+  goSetings: function () {
     if (!wx.getStorageSync('openid')) {
       if (!wx.getStorageSync('openid')) {
         wx.navigateTo({
@@ -154,21 +154,21 @@ Page({
     })
   },
   // 拨打商家电话
-  call(){
+  call() {
     wx.makePhoneCall({
       phoneNumber: '1340000' //仅为示例，并非真实的电话号码
     })
   },
   // 推荐列表
-  appgetrecomlist(){
+  appgetrecomlist() {
     var _this = this;
     call.getData('/app/goods/appgetrecomlist', {
       DB_STORE_ID: wx.getStorageSync('DB_STORE_ID'),
-      PULLNUM:'0'
+      PULLNUM: '0'
     }, function (res) {
-      if(res.state == "success"){
+      if (res.state == "success") {
         _this.setData({
-          recomlist:res.goods
+          recomlist: res.goods
         })
       }
       console.log(res);
@@ -186,7 +186,7 @@ Page({
       return;
     }
     wx.navigateTo({
-      url: '/pages/goods/detail/detail?id='+e.currentTarget.dataset.id
+      url: '/pages/goods/detail/detail?id=' + e.currentTarget.dataset.id
     })
   },
   goIntegral(e) {
@@ -201,7 +201,22 @@ Page({
       return;
     }
     wx.navigateTo({
-      url: '/pages/my/integral/index?num='+this.data.userInfo.U_INTEGRAL
+      url: '/pages/my/integral/index?num=' + this.data.userInfo.U_INTEGRAL
+    })
+  },
+  goCoupon(e) {
+    if (!wx.getStorageSync('openid')) {
+      if (!wx.getStorageSync('openid')) {
+        wx.navigateTo({
+          url: '/pages/login/login',
+          url: `/pages/login/login?from=${this.route}&tab=true`,
+        })
+        return;
+      }
+      return;
+    }
+    wx.navigateTo({
+      url: '/pages/my/coupons/list'
     })
   }
 })
